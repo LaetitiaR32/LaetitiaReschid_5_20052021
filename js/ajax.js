ff48c491280602420333f8773e090cbcce745a5f
Laetitia <laetitia.reschid@hotmail.fr>
2021-05-27 10:23:56 -0400

    test javascript
    mise en place ajax/page js orinoco

---
+++ js/ajax.js
@@ -0,0 +1,3 @@
+// GET : demnder à obtenir une information//
+
+// POST : demander la création d'une nouvelle donnée //
---
+++ js/orinoco.js
@@ -0,0 +1,16 @@
+/* images appareil photo*/ 
+let camImages =  [ 'images/vcam_1.jpg', 'vcam_2.jpg', 'vcam_3.jpg','vcam_4.jpg','vcam_5.jpg' ];
+
+function demarrage() {
+    camImages = document.createElement('img');
+    img.src = camImages[0];
+    let maDiv = document.getElementsByClassName("camera_picture1");
+    maDiv.appendChild(img);
+}  
+
+let sImages =  [ 'pierre','paul','jean'];
+
+
+
+
+
