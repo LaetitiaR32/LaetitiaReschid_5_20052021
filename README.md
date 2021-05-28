c039ff58a441957f5f6907d6f84b5a12d05e6b9b
Laetitia <laetitia.reschid@hotmail.fr>
2021-05-28 14:38:26 -0400

    acces localhost

---
+++ README.md
@@ -0,0 +1,4 @@
+# API 
+http://localhost:3000/api/cameras
+
+# Orinonco
--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,16 @@
 // GET : demnder à obtenir une information//
 
+let request = new XMLHttpRequestUpload ();
+
+resquest.onreadystatechange = function () {
+    if(this.readystate == 4 && this.status == 200){   //requete s'est bien passée//
+// les traitements à effectuer quand on a la réponse//
+}
+}
+//message de requete//
+request.open("GET","images/v_cam1.jpg");
+request.send(); // envoie de la requete//
+
+
+
 // POST : demander la création d'une nouvelle donnée //
