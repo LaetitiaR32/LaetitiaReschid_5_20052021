ad17b8054f3b2ff78c5c32dc2bf1f9c3926a69dc
Laetitia <laetitia.reschid@hotmail.fr>
2021-06-14 09:02:52 -0400

    test page d'accueil

--- js/orinoco.js
+++ js/orinoco.js
@@ -1,20 +1,65 @@
+fetch("http://localhost:3000/api/cameras") //recuperer l'info//
+    .then(function (response) {
+        return response.json()  // retourne promesse json
+    }).then(function (data) { //résultat reponse .json
+        console.log(data);
+        for(let i = 0; data.length; i++); 
 
-//affichage textes et photos
-const affichage_imageUrl = document.querySelector("#imageUrl");  
-const affichage_name = document.querySelector("#name");
-const affichage_description = document.querySelector("#description");
-const affichage_lenses = document.querySelector("#lenses");
-const affichage_price = document.querySelector("#price");
+        try {
+            displayProduct(data)
+            /*  const imageUrl = data[0].imageUrl;
+              const name = data[0].name;
+              const description = data[0].description;
+              const lenses = data[0].lenses;
+              const price = data[0].price; 
 
-affichage_name.innerHTML = name;
-affichage_description.innerHTML = description;
-affichage_lenses.innerHTML = lenses;
-affichage_price.innerHTML = price;
+              //afficher données
+              console.log(imageUrl);
+              console.log(name);
+              console.log(description);
+              console.log(lenses);
+              console.log(price);*/
 
-//affichage photos
-const image0_imageUrl = '<img src="${http://localhost:3000/images/cameras}">'
+        } catch (err) {
+            console.log(err);
+        }
 
-affichage_imageUrl.insertAdjacentHTML("afterbegin",image0_imageUrl)
+    });
+
+function displayProduct(product) { //déclaration de commentaire
+    console.log(product);   
+}
+//for(let i = 0; displayProduct.length; i++); {   // reperter le tableau 
+let div = document.createElement("div");// création div
+div.classList.add("list_product");// ajout class a ma div
+let img = document.createElement("img");// création des éléments img et p dans ma div
+let p = document.createElement("p");
+
+//insérer les valeurs
+    p.textContent = cameras[i].name;
+    p.textContent = cameras[i].description;
+    p.textContent = cameras[i].lenses;
+    p.textContent = cameras[i].price;
+
+//enfants 
+document.getElementById("main").appendChild(div);
+div.appendChild(img); // image enfant de ma div
+div.appendChild(p);
+
+
+
+
+
+/*document.getElementById('imageUrl').innerHTML  = imageUrl[0];
+document.getElementById('name').innerHTML = name[0];
+document.getElementById('description').innerHTML = description[0];
+document.getElementById('lenses').innerHTML = lenses[0];
+document.getElementById('price').innerHTML = price[0];
+
+for(let i = 0; i < getData.length; i++) {
+    console.log("data");
+}  */
+
 
 
 
